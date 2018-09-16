9bba7e5684aa84360ce10ff87263916e4491212f
Trivikram244 <trivikram244@gmail.com>
2018-09-16 11:43:19 +0530

    Animation

--- js/script.js
+++ js/script.js
@@ -1,41 +1,3 @@
-// Selecting DOM
-const menu_btn = document.querySelector('.menu-btn');
-const menu_btn_inner = document.querySelector('.menu-btn-inner');
-const menu = document.querySelector('.menu');
-const menu_branding = document.querySelector('.menu-branding');
-const menu_nav = document.querySelector('.menu-nav');
-const nav_items  = document.querySelectorAll('.nav-item');
-const nav_links  = document.querySelectorAll('.nav-link');
-
-
-// Set initial State of menu
-
-let showMenu = false;
-
-menu_btn_inner.addEventListener('click', toggleMenu);
-
-function toggleMenu(){
-  if(!showMenu){
-    menu_btn_inner.classList.add('close');
-    menu_btn.classList.add('whitebg');
-    menu.classList.add('show');
-    menu_nav.classList.add('show');
-    menu_branding.classList.add('show');
-    nav_items.forEach(item => item.classList.add('show'));
-    showMenu = true;
-  }else{
-    menu_btn_inner.classList.remove('close');
-    menu_btn.classList.remove('whitebg');
-    menu.classList.remove('show');
-    menu_nav.classList.remove('show');
-    menu_branding.classList.remove('show');
-    nav_items.forEach(item => item.classList.remove('show'));
-    showMenu = false;
-  }
-}
-
-nav_links.forEach(item => item.addEventListener("click", toggleMenu));
-
 
 // onscroll header background will change
 
@@ -99,3 +61,43 @@ $('.post').addClass("hidden").viewportChecker({
   }
   document.addEventListener('DOMContentLoaded', loadbar, false);
 }());
+
+
+
+// Selecting DOM
+const menu_btn = document.querySelector('.menu-btn');
+const menu_btn_inner = document.querySelector('.menu-btn-inner');
+const menu = document.querySelector('.menu');
+const menu_branding = document.querySelector('.menu-branding');
+const menu_nav = document.querySelector('.menu-nav');
+const nav_items  = document.querySelectorAll('.nav-item');
+const nav_links  = document.querySelectorAll('.nav-link');
+
+
+// Set initial State of menu
+
+let showMenu = false;
+
+menu_btn_inner.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+  if(!showMenu){
+    menu_btn_inner.classList.add('close');
+    menu_btn.classList.add('whitebg');
+    menu.classList.add('show');
+    menu_nav.classList.add('show');
+    menu_branding.classList.add('show');
+    nav_items.forEach(item => item.classList.add('show'));
+    showMenu = true;
+  }else{
+    menu_btn_inner.classList.remove('close');
+    menu_btn.classList.remove('whitebg');
+    menu.classList.remove('show');
+    menu_nav.classList.remove('show');
+    menu_branding.classList.remove('show');
+    nav_items.forEach(item => item.classList.remove('show'));
+    showMenu = false;
+  }
+}
+
+nav_links.forEach(item => item.addEventListener("click", toggleMenu));
